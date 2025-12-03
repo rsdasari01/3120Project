25fe2146eeb388da45c2ba49c191347a468ba12f
Adi <pchettri@charlotte.edu>
2025-12-03 12:21:58 -0500

    added horizontal stroke

--- printersim_v1.py
+++ printersim_v1.py
@@ -27,30 +27,34 @@ T3 = D / v_z                                            # time to hit wall
 firing_period = Tc
 V_step = ( ( (q*Tc)/(m*w) ) * (Tc/2 + (T3 - T2)) * dpm ) ** -1          # necessary voltage step for 300 dpi
 
+line_height = .006                                                  # length of vertical line
+line_width = .002                                                   # length of horizontal line
+N1 = 2 * (np.floor(dpm * line_height).astype(int) + 1)              # number of dots required for vertical
+N2 = np.floor(dpm * line_width).astype(int) + 1                     # number of dots required for horizontal
+N_TOTAL = N1 + N2
+T_TOTAL = (T3 + (N_TOTAL - 1) * firing_period)                      # total time for all droplets to finish
+NUM_POINTS = 2000                                                   # resolution
+t_global = np.linspace(0, T_TOTAL, NUM_POINTS)                 # time vector containing stretching to end of simulation time
 
-
-length_to_draw = .006                                                   # length of line
-N = 2*(np.floor(dpm * length_to_draw).astype(int) + 1)                                # number of dots required
-T_TOTAL = (T3 + (N - 1) * firing_period)                                  # total time for all droplets to finish
-NUM_POINTS = 1000
-t_global = np.linspace(0, T_TOTAL, NUM_POINTS)
-
+# converts t values to appropriate index
 def get_index(time):
     index = np.floor(time * NUM_POINTS/T_TOTAL).astype(int)
     return index
 
-x_array = np.zeros((N, NUM_POINTS))
-y_array = np.zeros((N, NUM_POINTS))
-z_array = np.zeros((N, NUM_POINTS)) + D
+# initialize arrays
+x_array = np.zeros((N_TOTAL, NUM_POINTS))
+y_array = np.zeros((N_TOTAL, NUM_POINTS))
+z_array = np.zeros((N_TOTAL, NUM_POINTS)) + D
 
-for i in range(N):
-    if i < N//2:
-        sign = 1
+for i in range(N_TOTAL):
+    if i < N1//2:
+        sign = -1
         shift = 0
     else:
-        sign = -1
-        shift = N//2
-    # Time
+        sign = 1
+        shift = N1 // 2
+
+    # time
     index0 = get_index(firing_period * i)
     index1 = get_index(firing_period * i + T1)
     index2 = get_index(firing_period * i + T2)
@@ -59,13 +63,13 @@ for i in range(N):
     t_local1 = t_global[index1:index2] - t_global[index1]
     t_local2 = t_global[index2:index3] - t_global[index2]
 
-    # Kinematics
+    # x kinematics
     Vx = sign * (0.001 * m * w) / ((q * Tc) * (0.5 * Tc + T3 - T2))
     Ex = Vx / w
     vx_exit = (q * Ex * Tc) / m
     x_exit = (q * Ex * Tc ** 2) / (2 * m)
-
-    Vy = V_step * (i - shift - np.floor(N / 4))
+    # y kinematics
+    Vy = V_step * (i - shift - np.floor(N1 / 4))
     Ey = Vy / w
     vy_exit = (q * Ey * Tc) / m
     y_exit = (q * Ey * Tc ** 2) / (2 * m)
@@ -76,22 +80,38 @@ for i in range(N):
     z_vector[index3:] = 0
     z_array[i] = z_vector
 
-    # y positions
-    y_vector = y_array[i]
-    y_vector[index1: index2] = (q * Ey) / (2 * m) * t_local1 ** 2
-    y_vector[index2: index3] = vy_exit * t_local2 + y_exit
-    y_vector[index3:] = y_vector[index3 - 1]
-    y_array[i] = y_vector
+    if i < N1:          # first half of loop is for the two vertical strokes
+        # x positions
+        x_vector = x_array[i]
+        x_vector[index1: index2] = (q * Ex) / (2 * m) * t_local1 ** 2
+        x_vector[index2: index3] = vx_exit * t_local2 + x_exit
+        x_vector[index3:] = x_vector[index3 - 1]
+        x_array[i] = x_vector
 
-    # x positions
-    x_vector = x_array[i]
-    x_vector[index1: index2] = (q * Ex) / (2 * m) * t_local1 ** 2
-    x_vector[index2: index3] = vx_exit * t_local2 + x_exit
-    x_vector[index3:] = x_vector[index3 - 1]
-    x_array[i] = x_vector
+        # y positions
+        y_vector = y_array[i]
+        y_vector[index1: index2] = (q * Ey) / (2 * m) * t_local1 ** 2
+        y_vector[index2: index3] = vy_exit * t_local2 + y_exit
+        y_vector[index3:] = y_vector[index3 - 1]
+        y_array[i] = y_vector
+
+    else:                   # horizontal stroke
+        shift = N1
+        Vx = V_step * (i - shift - np.floor(N2 / 2))
+        Ex = Vx / w
+        vx_exit = (q * Ex * Tc) / m
+        x_exit = (q * Ex * Tc ** 2) / (2 * m)
+
+        # x positions
+        x_vector = x_array[i]
+        x_vector[index1: index2] = (q * Ex) / (2 * m) * t_local1 ** 2
+        x_vector[index2: index3] = vx_exit * t_local2 + x_exit
+        x_vector[index3:] = x_vector[index3 - 1]
+        x_array[i] = x_vector
+
+
 
 dt = t_global[1] - t_global[0]
-N_drops = x_array.shape[0]
 
 fig = plt.figure(figsize=(8, 6))
 ax = fig.add_subplot(111, projection='3d')
